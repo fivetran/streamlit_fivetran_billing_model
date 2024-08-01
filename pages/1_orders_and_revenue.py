058b8f88416e41a422c15f00131105c0d1768c25
Renee Li <renee.li@fivetran.com>
2024-08-01 13:39:52 -0400

    update

--- pages/1_orders_and_revenue.py
+++ pages/1_orders_and_revenue.py
@@ -222,7 +222,7 @@ with col1:
         st.markdown("**New Customers Over Time**")
         
         # Apply the same date filter as used in other charts
-        filtered_data = data[(data['created_at'] >= min_created_at) & (data['created_at'] <= max_created_at)]
+        filtered_data = data[(data['customer_created_at'] >= min_created_at) & (data['customer_created_at'] <= max_created_at)]
         
         filtered_data['customer_created_month'] = filtered_data['customer_created_at'].dt.to_period('M').dt.to_timestamp()
         new_customers_over_time = filtered_data.groupby('customer_created_month')['customer_id'].nunique().reset_index()
